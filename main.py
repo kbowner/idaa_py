c810fc2b52b4adf3c944f7f917d7dc4d7b5c1f92
Vladimir <gomelcity@gmail.com>
2021-03-22 12:47:00 +0300

    Add: remove 3 last audit columns from PDA header if no audit cols in DB2 view

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ f_pda_list = "C:\PERSONAL\PYTHON_PRG\pda_view_list.txt"
 output_directory = "d:\Work\IDAA\OUT"
 fLog = "IDAA_session.out"
 
+# Dictionary to check and compare audit columns in both views PDA and DB2
+audit_col_status_code = {
+    1: 'Audit columns: YES (PDA and DB2); Last 3 columns and order are the same.',
+    2: 'Audit columns: YES (PDA and DB2); Last 3 columns are the same, but the order is different.',
+    3: 'Audit columns: NO (PDA and DB2); Both views have the same num of column.',
+    4: 'Audit columns: NO (PDA and DB2); Both views column count is different!',
+    5: 'Audit columns: YES (DB2) and NO (PDA); Both views column count is different!',
+    6: 'Audit columns: NO (DB2) and YES (PDA); Both views column count is different!',
+    7: 'Audit columns: YES (DB2) and NO (PDA); But views column count is the same!',
+    8: 'Audit columns: NO (DB2) and YES (PDA); But views column count is the same!',
+    9: 'Check columns and fix header/body of the view!',
+    10: 'Audit columns: NO (DB2) and YES (PDA); Both views column count is different! Will remove 3 last audit columns.'}
+# in case of it.6 and if a difference are audit columns list, then need to remove last 3 columns from the view header
+
 def IDAA_view_to_file (pda_schema, pda_view, bmsiw_schema, bmsiw_view):
 
     fname = pda_schema.upper() + "." + pda_view.upper()
@@ -32,7 +46,11 @@ def IDAA_view_to_file (pda_schema, pda_view, bmsiw_schema, bmsiw_view):
     f.write(f"-- # Legacy view: {bmsiw_schema:>20}.{bmsiw_view}\n")
     f.write("-- " + "#" * 80+"\n")
     f.write("\n")
-    idaa_view_header = print_idaa_view_header(nz_schema, nz_view)
+
+    level_audit_cols = 0
+    level_audit_cols = get_view_row_count('PDA', nz_schema, nz_view) - get_view_row_count('DB2', bmsiw_schema,
+                                                                                          bmsiw_view)
+    idaa_view_header = print_idaa_view_header(nz_schema, nz_view, level_audit_cols)
     idaa_view_statement = get_bmsiw_view_body(bmsiw_schema, bmsiw_view)
     cur_sqlid = "SIWASDBA"
     idaa_label = "\nLABEL ON TABLE " + pda_schema.upper() + "." + pda_view.upper() + " IS 'IDAA " + pda_schema.upper() + "';\n"
@@ -57,7 +75,7 @@ def get_legacy_view_name(pda_schema, pda_view):
         return tuple[0], tuple[1]
         tuple = ibm_db.fetch_tuple(stmt)
 
-def get_idaa_view_header(pda_schema, pda_view):
+def get_idaa_view_header(pda_schema, pda_view,level_audit_cols):
     sql = f"SELECT COLNAME||',' FROM IDAA.PDA_REL_COLUMNS WHERE CREATOR = '{pda_schema}' AND NAME = '{pda_view}' ORDER BY COLNO;"
     stmt = ibm_db.exec_immediate(conn, sql)
     s2 = ibm_db.fetch_tuple(stmt)
@@ -67,6 +85,11 @@ def get_idaa_view_header(pda_schema, pda_view):
         s2 = ibm_db.fetch_tuple(stmt)
     s1 = "CREATE VIEW " + pda_schema + "." + pda_view + " ("
     col_list_len = len(cols)
+
+    #remove 3 last audit columns in header
+    if level_audit_cols == 3:
+      col_list_len = col_list_len - 3
+
     col_list = []
     for i in range (0,col_list_len-1):
         if (i<col_list_len-1):
@@ -76,9 +99,9 @@ def get_idaa_view_header(pda_schema, pda_view):
     s3 = ")"
     return s1, col_list, s3
 
-def print_idaa_view_header(schema, view):
+def print_idaa_view_header(schema, view, level_audit_cols):
     col_list = []
-    header_begin, col_list, header_end = get_idaa_view_header(schema, view)
+    header_begin, col_list, header_end = get_idaa_view_header(schema, view, level_audit_cols)
     s = ""
     for items in col_list:
         s = s + " " + items
@@ -132,36 +155,40 @@ def check_audit_col (src_db, schema, view):
     aud_col_list.reverse()
     return aud_col_list
 
-def compare_audit_col(db2_list, pda_list):
-    msg = ""
+
+def compare_audit_col2(db2_list, pda_list):
+    res = 0
 
     # Now check the order of the audit columns
     if (all(x in db2_list for x in pda_list)) and (db2_list == standard_audit_col_list) and (db2_list == pda_list):
-        msg = "Audit columns: YES (PDA and DB2); Last 3 columns and order are the same."
+        res = 1
     elif (all(x in db2_list for x in pda_list)) and (db2_list == standard_audit_col_list) and (db2_list != pda_list):
-        msg = "Audit columns: YES (PDA and DB2); Last 3 columns are the same, but the order is different."
+        res = 2
     elif (db2_list != standard_audit_col_list) and (pda_list != standard_audit_col_list) \
             and (get_view_row_count('DB2', bmsiw_schema, bmsiw_view) == get_view_row_count('PDA', nz_schema, nz_view)):
-        msg = "Audit columns: NO (PDA and DB2); Both views have the same num of column."
+        res = 3
     elif (db2_list != standard_audit_col_list) and (pda_list != standard_audit_col_list) \
             and (get_view_row_count('DB2', bmsiw_schema, bmsiw_view) != get_view_row_count('PDA', nz_schema, nz_view)):
-        msg = "Audit columns: NO (PDA and DB2); Both views column count is different!"
+        res = 4
     elif (db2_list == standard_audit_col_list) and (pda_list != standard_audit_col_list) \
             and (get_view_row_count('DB2', bmsiw_schema, bmsiw_view) != get_view_row_count('PDA', nz_schema, nz_view)):
-        msg = "Audit columns: YES (DB2) and NO (PDA); Both views column count is different!"
+        res = 5
     elif (db2_list != standard_audit_col_list) and (pda_list == standard_audit_col_list) \
             and (get_view_row_count('DB2', bmsiw_schema, bmsiw_view) != get_view_row_count('PDA', nz_schema, nz_view)):
-        msg = "Audit columns: NO (DB2) and YES (PDA); Both views column count is different!"
+        res = 6
+        # if a difference are audit columns list, then need to remove last 3 columns from the view header
+        if (get_view_row_count('PDA', nz_schema, nz_view) - get_view_row_count('DB2', bmsiw_schema, bmsiw_view)) == 3:
+            res = 10
     elif (db2_list == standard_audit_col_list) and (pda_list != standard_audit_col_list) \
             and (get_view_row_count('DB2', bmsiw_schema, bmsiw_view) == get_view_row_count('PDA', nz_schema, nz_view)):
-        msg = "Audit columns: YES (DB2) and NO (PDA); But views column count is the same!"
+        res = 7
     elif (db2_list != standard_audit_col_list) and (pda_list == standard_audit_col_list) \
             and (get_view_row_count('DB2', bmsiw_schema, bmsiw_view) == get_view_row_count('PDA', nz_schema, nz_view)):
-        msg = "Audit columns: NO (DB2) and YES (PDA); But views column count is the same!"
+        res = 8
     else:
-        msg = "Check columns and fix header/body of the view!"
+        res = 9
 
-    return msg
+    return res
 
 def IDAA_proceed_single_view (nz_schema, nz_view,  bmsiw_schema, bmsiw_view):
 
@@ -180,7 +207,6 @@ def IDAA_proceed_single_view (nz_schema, nz_view,  bmsiw_schema, bmsiw_view):
     # print(sqlparse.format(idaa_view_statement, reindent=True, wrap_after=True, truncate_strings=80))
     # ----------------
 
-
     log_print("-- " + "#" * 80)
     log_print(f"-- # PDA view: {nz_schema:>23}.{nz_view}")
     log_print(f"-- # Legacy view: {bmsiw_schema:>20}.{bmsiw_view}")
@@ -188,16 +214,16 @@ def IDAA_proceed_single_view (nz_schema, nz_view,  bmsiw_schema, bmsiw_view):
     log_print(f"-- # Legacy view column count = {get_view_row_count('DB2', bmsiw_schema, bmsiw_view):>5}")
     log_print(f"-- # PDA view column count = {get_view_row_count('PDA', nz_schema, nz_view):>8}")
     log_print("-- #")
-    log_print("-- # " + compare_audit_col(check_audit_col ('DB2', bmsiw_schema, bmsiw_view),check_audit_col ('PDA', nz_schema, nz_view)))
+    log_print("-- # " + audit_col_status_code [compare_audit_col2(check_audit_col('DB2', bmsiw_schema, bmsiw_view), check_audit_col('PDA', nz_schema, nz_view))])
     log_print("")
+
     # First check if there are records for PDA view
-    if (get_view_row_count('PDA', nz_schema, nz_view)>0):
+    if get_view_row_count('PDA', nz_schema, nz_view) > 0:
         IDAA_view_to_file (nz_schema, nz_view, bmsiw_schema, bmsiw_view)
     else:
         log_print("ERROR: No rows for PDA view found!")
     log_print("-- " + "#" * 80)
     log_print("")
-
 
 
 if __name__ == '__main__':
@@ -208,21 +234,20 @@ if __name__ == '__main__':
         os.remove(flog_abs)
 
     # -- example - how to proceed with a single view -----------------------
-    # nz_schema = 'ACCTRCVBL'
-    # nz_view = 'TREASURY_ITEM_UV'
-    # bmsiw_schema, bmsiw_view = get_legacy_view_name(nz_schema, nz_view)
-    # IDAA_proceed_single_view (nz_schema, nz_view, bmsiw_schema, bmsiw_view)
+    nz_schema = 'CMNREF'
+    nz_view = 'LOCAL_COLUMN_AUDIT_V'
+    bmsiw_schema, bmsiw_view = get_legacy_view_name(nz_schema, nz_view)
+    IDAA_proceed_single_view (nz_schema, nz_view, bmsiw_schema, bmsiw_view)
     # ----------------------------------------------------------------------
 
-    sql = f"SELECT NEW_SCHEMA, NEW_VIEW_NAME FROM IDAA.PDA_VIEW_MAP;"
-    stmt = ibm_db.exec_immediate(conn, sql)
-    tuple = ibm_db.fetch_tuple(stmt)
-    while tuple:
-        nz_schema = tuple[0]
-        nz_view = tuple[1]
-        tuple = ibm_db.fetch_tuple(stmt)
-        bmsiw_schema, bmsiw_view = get_legacy_view_name(nz_schema, nz_view)
-        IDAA_proceed_single_view(nz_schema, nz_view, bmsiw_schema, bmsiw_view)
-
+    #sql = f"SELECT NEW_SCHEMA, NEW_VIEW_NAME FROM IDAA.PDA_VIEW_MAP;"
+    #stmt = ibm_db.exec_immediate(conn, sql)
+    #tuple = ibm_db.fetch_tuple(stmt)
+    #while tuple:
+    #    nz_schema = tuple[0]
+    #    nz_view = tuple[1]
+    #    tuple = ibm_db.fetch_tuple(stmt)
+    #    bmsiw_schema, bmsiw_view = get_legacy_view_name(nz_schema, nz_view)
+    #    IDAA_proceed_single_view(nz_schema, nz_view, bmsiw_schema, bmsiw_view)
 
 
